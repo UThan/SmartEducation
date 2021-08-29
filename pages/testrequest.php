fc092b88206fb47fc9be0ba158b731306813f4fb
Yu Than <yuthan.yt@gmail.com>
2021-08-29 18:12:21 +0630

    new laravel application installed

--- pages/testrequest.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "smarxieu_admin";
-$password = ",Sh.9(co6pDN";
-$dbname = "smarxieu_agent_portal";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    exit(json_encode($_POST));
-}
